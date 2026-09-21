8b52faaee59698dd7a26cf7eb2ecfcb8fc10f4ad
yasintake23-tech <yasintake23@gmail.com>
2026-09-21 23:17:32 +0300

    fix: resolve Compose compilation issues in Libra

--- app/src/main/java/com/libra/app/feature/home/HomeScreen.kt
+++ app/src/main/java/com/libra/app/feature/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
