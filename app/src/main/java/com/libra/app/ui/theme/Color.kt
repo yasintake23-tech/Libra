a61000a789a2cc778c089cbfe31e1c9dd3c9627a
yasintake23-tech <yasintake23@gmail.com>
2026-09-21 22:36:47 +0300

    ui: establish Libra monochrome theme

--- app/src/main/java/com/libra/app/ui/theme/Color.kt
+++ app/src/main/java/com/libra/app/ui/theme/Color.kt
@@ -2,28 +2,16 @@ package com.libra.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Warm Editorial Book Palette
-val Terracotta = Color(0xFFD9532F)
-val TerracottaLight = Color(0xFFFF865C)
-val TerracottaDark = Color(0xFFA12B0E)
-
-val InkNavy = Color(0xFF1A2238)
-val InkNavyLight = Color(0xFF334155)
-val InkNavyDark = Color(0xFF0F172A)
-
-val WarmAmber = Color(0xFFD97706)
-val SageGreen = Color(0xFF3B7A57)
-
-val ParchmentLight = Color(0xFFFDFBF7)
-val ParchmentCardLight = Color(0xFFFFFFFF)
-val ParchmentBorderLight = Color(0xFFEAE5D9)
-
-val MidnightDark = Color(0xFF0F141C)
-val MidnightCardDark = Color(0xFF18202C)
-val MidnightBorderDark = Color(0xFF283548)
-
-val SlateTextLight = Color(0xFF1E293B)
-val SlateMutedLight = Color(0xFF64748B)
-
-val SlateTextDark = Color(0xFFF1F5F9)
-val SlateMutedDark = Color(0xFF94A3B8)
+val LibraInk = Color(0xFF171717)
+val LibraInkSoft = Color(0xFF3F3F46)
+val LibraMuted = Color(0xFF71717A)
+val LibraLine = Color(0xFFE4E4E7)
+val LibraSurface = Color(0xFFFFFFFF)
+val LibraBackground = Color(0xFFFAFAF9)
+val LibraAccent = Color(0xFF27272A)
+val LibraAccentSoft = Color(0xFFE7E5E4)
+val LibraDarkBackground = Color(0xFF111111)
+val LibraDarkSurface = Color(0xFF1A1A1A)
+val LibraDarkText = Color(0xFFF5F5F4)
+val LibraDarkMuted = Color(0xFFA1A1AA)
+val LibraDarkLine = Color(0xFF2A2A2A)
