07d475e2ca1d1e6a0243537952d029d89149404b
yasintake23-tech <yasintake23@gmail.com>
2026-09-22 14:01:15 +0300

    fix: import bottom navigation weight modifier

--- app/src/main/java/com/libra/app/ui/navigation/BottomNavBar.kt
+++ app/src/main/java/com/libra/app/ui/navigation/BottomNavBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
