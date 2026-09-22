05bb58e9b15314f5547fa844a68b6cb5209e9d1c
yasintake23-tech <yasintake23@gmail.com>
2026-09-22 17:52:55 +0300

    feat: allow creating notifications

--- app/src/main/java/com/libra/app/domain/repository/NotificationRepository.kt
+++ app/src/main/java/com/libra/app/domain/repository/NotificationRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface NotificationRepository {
     fun observeNotifications(uid: String, limit: Long = 50): Flow<AppResult<List<AppNotification>>>
+    suspend fun create(notification: AppNotification): AppResult<Unit>
     suspend fun markRead(notificationId: String): AppResult<Unit>
     suspend fun markAllRead(uid: String): AppResult<Unit>
 }
