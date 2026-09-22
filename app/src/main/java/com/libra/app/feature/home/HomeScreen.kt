c0f57a37b059965386d78bd522ab8a1b0bce7025
yasintake23-tech <yasintake23@gmail.com>
2026-09-22 20:36:52 +0300

    fix: restore missing home layout imports

--- app/src/main/java/com/libra/app/feature/home/HomeScreen.kt
+++ app/src/main/java/com/libra/app/feature/home/HomeScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
