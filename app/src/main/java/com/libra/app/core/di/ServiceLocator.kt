5544069db2eb51975002ddc4b7b6019207180ca7
yasintake23-tech <yasintake23@gmail.com>
2026-09-23 10:48:12 +0300

    refactor: wire story repository

--- app/src/main/java/com/libra/app/core/di/ServiceLocator.kt
+++ app/src/main/java/com/libra/app/core/di/ServiceLocator.kt
@@ -7,6 +7,7 @@ import com.libra.app.data.chat.RealtimeChatRepositoryImpl
 import com.libra.app.data.community.FirebaseCommunityRepositoryImpl
 import com.libra.app.data.post.FirebasePostRepositoryImpl
 import com.libra.app.data.storage.CloudflareR2StorageRepositoryImpl
+import com.libra.app.data.story.FirebaseStoryRepositoryImpl
 import com.libra.app.data.user.FirebaseUserRepositoryImpl
 import com.libra.app.data.notification.FirebaseNotificationRepositoryImpl
 import com.libra.app.domain.repository.AiAssistantRepository
@@ -16,6 +17,7 @@ import com.libra.app.domain.repository.ChatRepository
 import com.libra.app.domain.repository.CommunityRepository
 import com.libra.app.domain.repository.PostRepository
 import com.libra.app.domain.repository.StorageRepository
+import com.libra.app.domain.repository.StoryRepository
 import com.libra.app.domain.repository.UserRepository
 import com.libra.app.domain.repository.NotificationRepository
 
@@ -26,6 +28,7 @@ object ServiceLocator {
     val communityRepository: CommunityRepository by lazy { FirebaseCommunityRepositoryImpl(userRepository) }
     val chatRepository: ChatRepository by lazy { RealtimeChatRepositoryImpl(userRepository, storageRepository, notificationRepository) }
     val postRepository: PostRepository by lazy { FirebasePostRepositoryImpl(userRepository, storageRepository, notificationRepository) }
+    val storyRepository: StoryRepository by lazy { FirebaseStoryRepositoryImpl() }
     val notificationRepository: NotificationRepository by lazy { FirebaseNotificationRepositoryImpl() }
 
     val r2StorageRepository: CloudflareR2StorageRepositoryImpl by lazy {
