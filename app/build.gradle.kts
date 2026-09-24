9af66c2439da4b9ae413c22012be128e811e1b4d
yasintake23-tech <yasintake23@gmail.com>
2026-09-24 20:58:09 +0300

    chore: bump icon test build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.libra.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
